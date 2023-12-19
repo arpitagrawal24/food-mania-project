03a87441187e144f4a7a371f13b57d1c68822413
Arpit <arpitagrawal2402@gmail.com>
2023-12-19 12:56:06 +0530

    api change

--- src/utils/Api/useRestaurantMenu.js
+++ src/utils/Api/useRestaurantMenu.js
@@ -1,18 +1,20 @@
 import { useState, useEffect } from "react";
-import { FETCH_MENU_URL1, FETCH_MENU_URL2 } from "../../config";
+import { FETCH_MENU_URL1, FETCH_MENU_URL2, menudata } from "../../config";
 
 const useRestaurantMenu = (resId) => {
   const [restaurant, setRestaurant] = useState(null);
 
   useEffect(() => {
+    console.log(menudata);
     getRestaurantInfo();
   }, []);
 
   async function getRestaurantInfo() {
     try {
-      const data = await fetch(FETCH_MENU_URL1 + resId + FETCH_MENU_URL2);
-      const json = await data.json();
-      setRestaurant(json.data);
+      // const data = await fetch(FETCH_MENU_URL1 + resId + FETCH_MENU_URL2);
+      // const json = await data.json();
+      // setRestaurant(json.data);
+      setRestaurant(menudata ? menudata[0].data : null);
     } catch (error) {
       console.error("Error fetching data from the proxy server:", error);
       // setRestaurant(menudata[0].data);
--- src/utils/Api/useRestaurantsList.js
+++ src/utils/Api/useRestaurantsList.js
@@ -4,17 +4,21 @@ import { FETCH_RESTAURANTS_URL } from "../../config";
 const useRestaurantsList = () => {
   const [restaurants, setRestaurants] = useState([]);
 
+  
   useEffect(() => {
+    console.log(FETCH_RESTAURANTS_URL);
     getRestaurants(); // API call is made only once
   }, []);
 
   async function getRestaurants() {
     try {
-      const data = await fetch(FETCH_RESTAURANTS_URL);
-      const json = await data.json();
-      setRestaurants(
-        json?.data.cards[2]?.card?.card?.gridElements?.infoWithStyle.restaurants
-      );
+      // const data = await fetch(FETCH_RESTAURANTS_URL);
+      // const json = await data.json();
+      // setRestaurants(
+      //   json?.data.cards[2]?.card?.card?.gridElements?.infoWithStyle.restaurants
+      // );
+      setRestaurants(FETCH_RESTAURANTS_URL)
+      console.log(data);
     } catch (error) {
       console.error("Error fetching data from the api:", error);
       // setRestaurants(apidata);
