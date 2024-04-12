fa657563417ee683b462881ba5aa10209dc276e5
Arpit <arpitagrawal2402@gmail.com>
2024-04-12 20:53:44 +0530

    image fixed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const SERVER_URL = "https://food-mania.onrender.com/";
 
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 // export const FETCH_RESTAURANTS_URL =
 //   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
